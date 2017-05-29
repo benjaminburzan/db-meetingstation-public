de3093a764248dbf2cfe5056c050cfab833f1be4
Michael Zilske <michael.zilske@tu-berlin.de>
2017-05-29 16:56:09 +0200

    net travel time

--- reader-gtfs/src/main/java/com/graphhopper/reader/gtfs/GraphExplorer.java
+++ reader-gtfs/src/main/java/com/graphhopper/reader/gtfs/GraphExplorer.java
@@ -118,12 +118,24 @@ public final class GraphExplorer {
     }
 
     private long waitingTime(EdgeIteratorState edge, long earliestStartTime) {
-        return (flagEncoder.getTime(edge.getFlags()) - secondsOnTrafficDay(edge, earliestStartTime)) * 1000;
+        return flagEncoder.getTime(edge.getFlags()) * 1000 - secondsOnTrafficDay(edge, earliestStartTime) * 1000;
     }
 
     private int secondsOnTrafficDay(EdgeIteratorState edge, long instant) {
         final ZoneId zoneId = gtfsStorage.getTimeZones().get(flagEncoder.getValidityId(edge.getFlags())).zoneId;
-        return Instant.ofEpochMilli(instant).atZone(zoneId).toLocalTime().toSecondOfDay();
+        final int i = Instant.ofEpochMilli(instant).atZone(zoneId).toLocalTime().toSecondOfDay();
+        return i;
+    }
+
+    private long secondsOnTrafficDay2(EdgeIteratorState edge, long instant) {
+        final ZoneId zoneId = gtfsStorage.getTimeZones().get(flagEncoder.getValidityId(edge.getFlags())).zoneId;
+        final long l = Instant.ofEpochMilli(instant).atZone(zoneId).toLocalTime().toNanoOfDay();
+        final long i = l / 1000000L;
+        final long j = Instant.ofEpochMilli(instant).atZone(zoneId).toLocalTime().toSecondOfDay();
+        System.out.println(j);
+        System.out.println(i);
+        System.out.println(l);
+        return i;
     }
 
     private boolean isValidOn(EdgeIteratorState edge, long instant) {
